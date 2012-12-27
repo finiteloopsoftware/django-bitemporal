a9b33c866b8ce1f79d8997db70cf2819d3c2d4cc
Mike Crute <mcrute@gmail.com>
2012-12-27 17:02:17 -0500

    Prefix with valid to avoid name collisions

--- bitemporal/models.py
+++ bitemporal/models.py
@@ -6,8 +6,8 @@ class BitemporalModelBase(models.Model):
     id = models.IntegerField()
     row_id = models.AutoField(primary_key=True)
 
-    start_date = models.DateTimeField(auto_add_now=True)
-    end_date = models.DateTimeField(blank=True, null=True)
+    valid_start_date = models.DateTimeField(auto_add_now=True)
+    valid_end_date = models.DateTimeField(blank=True, null=True)
 
     txn_start_date = models.DateTimeField(auto_now_add=True)
     txn_end_date = models.DateTimeField(blank=True, null=True)
