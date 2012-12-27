a499721d47bf0226660af48031b89cfd4efcc985
Mike Crute <mcrute@gmail.com>
2012-12-27 18:16:30 -0500

    Add current method

--- bitemporal/models.py
+++ bitemporal/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from datetime import datetime
+from django.db.models import Q
+from django.db.models.query import QuerySet
+from django.db.models.manager import Manager
+
+
+class BitemporalQuerySet(QuerySet):
+
+    def current(self):
+        return self.filter(
+            valid_start_date__gte=datetime.now(),
+            Q(valid_end_date__lte=datetime.now()) | Q(valid_end_date=None))
+
+
+class BitemporalManager(Manager):
+
+    def get_query_set(self):
+        return BitemporalQuerySet(self.model, using=self._db)
 
 
 class BitemporalModelBase(models.Model):
