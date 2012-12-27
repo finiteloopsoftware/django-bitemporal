c889ce6728c52fefe4618738598521ef13bbba7f
Mike Crute <mcrute@gmail.com>
2012-12-27 16:47:24 -0500

    Add base model fields

--- bitemporal/models.py
+++ bitemporal/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class BitemporalModelBase(models.Model):
+
+    id = models.IntegerField()
+    row_id = models.AutoField(primary_key=True)
+
+    start_date = models.DateTimeField(auto_add_now=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+
+    txn_start_date = models.DateTimeField(auto_now_add=True)
+    txn_end_date = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+        unique_together = [
+            ('id', 'start_date', 'end_date', 'txn_end_date'),
+        ]
