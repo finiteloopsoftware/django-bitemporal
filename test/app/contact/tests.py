7044a1347a37cb96fddbbb4848210e60a78cbee0
Six <1970inazuma@gmail.com>
2012-12-27 18:03:06 -0500

    unit test for testing .get

--- test/app/contact/tests.py
+++ test/app/contact/tests.py
@@ -7,6 +7,8 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from contact import models
+import datetime
+
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -19,13 +21,36 @@ class SimpleTest(TestCase):
 class TestContact(TestCase):
 
     def setUp(self):
-        self.one = models.Contact(id=1, name=u"John Doe", is_organization=False)
-        self.two = models.Contact(id=2, name=u"Acme Corp", is_organization=True)
-        self.three = models.Contact(id=2, name=u"Acme LLC", is_organization=True)
+        obj = models.Contact(id=1,
+                name=u"John Doe",
+                is_organization=False,
+                valid_start_date=datetime.datetime(1980, 1, 1, 0, 0 ,0),
+                valid_end_date=None,
+                txn_start_date=datetime.datetime.now(),
+                txn_end_date=None)
+        obj.save()
 
-    def test_add(self):
-        self.one.save()
-        self.two.save()
+        obj = models.Contact(id=2,
+                name=u"Acme Corp",
+                is_organization=True,
+                valid_start_date=datetime.datetime(1980, 10, 6, 0, 0 ,0),
+                valid_end_date=None,
+                txn_start_date=datetime.datetime.now(),
+                txn_end_date=None)
+        obj.save()
+
+        end_date = datetime.datetime(1997, 5, 13, 0, 0, 0)
+
+        obj.txn_end_date=end_date
+        obj.save()
+        obj = models.Contact(id=2,
+                name=u"Acme LLC",
+                is_organization=True,
+                valid_start_date=end_date,
+                valid_end_date=None,
+                txn_start_date=datetime.datetime.now(),
+                txn_end_date=None)
+        obj.save()
 
     def test_get_john_doe(self):
         obj = models.Contact.objects.get(pk=1)
@@ -34,4 +59,3 @@ class TestContact(TestCase):
     def test_get_achme(self):
         obj = models.Contact.objects.get(pk=2)
         self.assertEqual(obj.name, u"Acme LLC")
-
