3edf91b7cb6725182681711aee4410e052e2f0b0
Mike Crute <mcrute@gmail.com>
2012-12-27 17:25:14 -0500

    Fix typo

--- bitemporal/models.py
+++ bitemporal/models.py
@@ -6,7 +6,7 @@ class BitemporalModelBase(models.Model):
     id = models.IntegerField()
     row_id = models.AutoField(primary_key=True)
 
-    valid_start_date = models.DateTimeField(auto_add_now=True)
+    valid_start_date = models.DateTimeField(auto_now_add=True)
     valid_end_date = models.DateTimeField(blank=True, null=True)
 
     txn_start_date = models.DateTimeField(auto_now_add=True)
