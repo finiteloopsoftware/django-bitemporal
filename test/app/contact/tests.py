ef366e236441e4e9cb5107b3b39bfc149ec7a6c7
Six <1970inazuma@gmail.com>
2012-12-27 17:45:20 -0500

    tests for .get

--- test/app/contact/tests.py
+++ test/app/contact/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from contact import models
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +14,24 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class TestContact(TestCase):
+
+    def setUp(self):
+        self.one = models.Contact(id=1, name=u"John Doe", is_organization=False)
+        self.two = models.Contact(id=2, name=u"Acme Corp", is_organization=True)
+        self.three = models.Contact(id=2, name=u"Acme LLC", is_organization=True)
+
+    def test_add(self):
+        self.one.save()
+        self.two.save()
+
+    def test_get_john_doe(self):
+        obj = models.Contact.objects.get(pk=1)
+        self.assertEqual(obj.name, u"John Doe")
+
+    def test_get_achme(self):
+        obj = models.Contact.objects.get(pk=2)
+        self.assertEqual(obj.name, u"Acme LLC")
+
